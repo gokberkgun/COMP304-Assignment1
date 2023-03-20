86d597be0b9878503a9ac51611e534153c3f8c27
Gökberk Gün <gokberkgun@192.168.1.35>
2023-03-20 21:25:50 +0300

    p2b

---
+++ ps2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+int main (int argc, char *argv[]) {
+
+  pid_t pid = fork()
+  
+  
+  if (pid == 0) {
+    sleep(5);
+  else {
+    exit(0);
+  }
+
+  return 0;
+
+}
