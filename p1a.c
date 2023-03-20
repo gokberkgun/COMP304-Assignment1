2775715a6790ae2041dade3e28a69a5d5bfc3607
Gökberk Gün <gokberkgun@192.168.1.35>
2023-03-20 19:03:13 +0300

    p1a.c

--- p1a.c
+++ p1a.c
@@ -9,20 +9,25 @@ void recursive_fork (int level, int n) {
   if (n==0) {
     return;
   }
-
-  for (int i = 0; i<3 ; i++) {
  
-    int pid = fork();
-    int ppid = getppid();
-
-    if (pid == 0) {
-      //child process
-      printf("Process ID: %d, Parent ID: %d, level: %d\n",getpid(),getppid(), level);
-      recursive_fork(level+1,n-1);
-      exit(0);
-    }        
-  }
-}
+  int pid = fork();
+  int ppid = getppid();
+   
+  if (pid == 0) {
+    //child process
+    printf("Process ID: %d, Parent ID: %d, level: %d\n",getpid(),getppid(), level);
+    recursive_fork(level+1,n-1);
+    exit(0);
+  } else if (pid < 0) {
+      //false fork
+      exit(1);
+  } else {
+      //parent process loop waits for all childs to exit
+      for (int i = 0 ; i<n ; i++) {
+        wait(NULL);
+      } 
+  }      
+} 
 
 int main (int argc) {
  
