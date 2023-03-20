251787f8ef2bbe31f9ebe4143ffeaf1c1c7140f3
Gökberk Gün <gokberkgun@192.168.1.35>
2023-03-20 18:06:27 +0300

    p1a.c

--- p1a.c
+++ p1a.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void recursive_fork (int level, int n) {
+
+  //base case
+  if (n==0) {
+    return;
+  }
+
+  int pid = fork();
+  int ppid = getppid();
+
+  if (pid == 0) {
+    //child process
+    printf("Process ID: %d, Parent ID: %d, level: %d\n",pid,ppid, level);
+    recursive_fork(level+1,n-1);
+    exit(0);
+  }
+}
+
 int main (int argc) {
  
   int pid;
@@ -15,21 +33,3 @@ int main (int argc) {
   return 0;
 
 } 
-
-void recursive_fork (int level, int n) {
-
-  //base case
-  if (n==0) {
-    return;
-  }
-
-  int pid = fork();
-  int ppid = getppid();
-  
-  if (pid == 0) {
-    //child process
-    printf("Process ID: %d, Parent ID: %d, level: %d\n",pid,ppid, level);
-    recursive_fork(level+1,n-1);
-    exit(0);
-  }
-}  
