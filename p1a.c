0cf554c4c434e3fc75c53fe72b4ccb35810966a5
Gökberk Gün <67122048+gokberkgun@users.noreply.github.com>
2023-03-20 20:34:09 +0300

    Update p1a.c

--- p1a.c
+++ p1a.c
@@ -12,7 +12,7 @@ int main (int argc) {
   pid_t pid = getpid();  
   printf("Main Process ID: %d, level: 0\n",pid);
   
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n+1; i++) {
 
     pid_t pid = fork();
 
