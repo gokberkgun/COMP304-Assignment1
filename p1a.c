c2f99a6e1825d01448f202bb6604db77b65310c6
Gökberk Gün <67122048+gokberkgun@users.noreply.github.com>
2023-03-20 20:23:08 +0300

    Update p1a.c

--- p1a.c
+++ p1a.c
@@ -2,26 +2,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-
-
-void recursive_fork (int level, int n) {
-
-  //base case
-  if (n==0) {
-    return;
-  }
-
-  int pid = fork();
-
-  if (pid < 0) {
-    //incorrect fork
-    exit(1);
-  } else if (pid == 0) {
-     //child process
-     printf("Process ID: %d, Parent ID: %d, level: %d\n",getpid(),getppid(), level);
-     recursive_fork(level + 1, n - 1);
-  }
-}      
  
 int main (int argc) {
  
@@ -31,8 +11,13 @@ int main (int argc) {
   //initial process
   pid = getpid();  
   printf("Main Process ID: %d, level: 0\n",pid);
+  
+  for (int i = 0 ; i<n ; i++) {
+    fork();
+  }
 
-  recursive_fork(1,n+1);
+  printf("Process ID: %d, Parent ID: %d, level: %d\n",getpid(),getppid(), level);
+  wait(NULL);
 
   return 0;
 
