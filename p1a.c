2fc8073c96e475f65900daa28d98dbf4cd8c60b5
Gökberk Gün <67122048+gokberkgun@users.noreply.github.com>
2023-03-20 13:03:58 +0300

    Update p1a.c

--- p1a.c
+++ p1a.c
@@ -6,7 +6,7 @@ int main (int argc) {
   int i;
   
   parentid = getpid();  
-  printf("Main Process ID: %d, level: %d",parentid, level) 
+  printf("Main Process ID: %d, level: %d",parentid, level);
 
   for(i=0 ; i<argc ; i++) {
     level += 1
@@ -14,7 +14,7 @@ int main (int argc) {
     pid = getpid();
     parentid = getppid();
     if (value == 0) {
-      printf("Process ID: %d, Parent ID: %d, level: %d",pid,parentid, level)
+      printf("Process ID: %d, Parent ID: %d, level: %d",pid,parentid, level);
     }
   }
 } 
