29a15eca0365ae89c445f78969a0c0f09e8f35e5
Gökberk Gün <67122048+gokberkgun@users.noreply.github.com>
2023-03-20 12:37:27 +0300

    Add files via upload

---
+++ p1a.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (int argc) {
+ 
+  int value, pid, parentid,level=0;
+  int i;
+  
+  parentid = getpid();  
+  printf("Main Process ID: %d, level: %d",parentid, level) 
+
+  for(i=0 ; i<argc ; i++) {
+    level += 1
+    value = fork();
+    pid = getpid();
+    parentid = getppid();
+    if (value == 0) {
+      printf("Process ID: %d, Parent ID: %d, level: %d",pid,parentid, level)
+    }
+  }
+} 
+
+  
