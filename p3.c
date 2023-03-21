591ea1ae92031392af0f34f4d400f61e9ee56991
Gökberk Gün <gokberkgun@192.168.1.35>
2023-03-21 21:36:32 +0300

    p3.c

---
+++ p3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+int main(int argc, char *argv[]) {
+  
+  define MAX_NUMBERS = 1000;
+ 
+  int x = atoi(argv[1]);
+  int n = atoi(argv[2]);
+ 
+  int sequence[MAX_NUMBERS];
+  int count = 0;
+  pid_t pids[n];
+  int status;
+  char buffer[1024];
+  while (fgets(buffer,1024,stdin) != NULL) {
+    int number = atoi(buffer);
+    sequence[count] = number;
+    count++;
+  }
+  int portion = count / n;
+  int remainder = count % n;
+
+  for (int i = 0 ; i < n ; i++) {
+    pid[i] = fork();
+    
+    if (pid == 0) {
+       int start = i * portion;
+       int end = (i + 1) * portion;
+       for (int j = start ; j < end ; j++) {
+         if (sequence[j] == x) {
+           printf(Found at index %d", j);
+           exit(0);
+         }
+       }
+     exit(1)
+    } else {
+        waitpid(pids[i], &status, 0);
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+          for (int k = 0 ; k < n ; k++) {
+            if (k != i) {
+              kill(pids[k], SIGTERM);
+            }
+          }
+        }
+      }
+  }        
+  
+  return 0;
+}
